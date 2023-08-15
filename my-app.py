6531fd04f2639a56a5bca60778b070a6d826aede
zoltanvacz <113783287+zoltanvacz@users.noreply.github.com>
2023-08-15 16:04:51 +0200

    Update my-app.py

--- my-app.py
+++ my-app.py
@@ -1,1 +1,10 @@
+from flask import Flask
+app = Flask(__name__)
 
+@app.route('/')
+def main():
+        version = "1.0"
+        return 'Version: ' +version
+
+if __name__ == '__main__':
+        app.run()
